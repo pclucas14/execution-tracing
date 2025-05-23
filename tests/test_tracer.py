30cb818ce0377d68d68f3d7b3aa3f1fddc487c2b
Lucas Caccia <lucas.page-caccia@mail.mcgill.ca>
2025-05-23 11:13:12 -0700

    test for external

--- tests/test_tracer.py
+++ tests/test_tracer.py
@@ -32,7 +32,8 @@ class TestTracer(unittest.TestCase):
         # Simulate a call from in-scope to out-of-scope
         self.tracer.log_function_call('out_of_scope_function', {'arg': 'value'}, 
                                     '/external/module.py', 42, 
-                                    ('/home/lpagecaccia/my_tracer/in_scope.py', 10), 0)
+                                    ('/home/lpagecaccia/my_tracer/in_scope.py', 10), 0, 
+                                    is_external=True)
         
         # Verify the call was logged
         self.assertEqual(len(self.tracer.log), 1)
@@ -40,6 +41,7 @@ class TestTracer(unittest.TestCase):
         self.assertEqual(logged_call['name'], 'out_of_scope_function')
         self.assertEqual(logged_call['location'], 'module.py:42')
         self.assertEqual(logged_call['arguments'], {'arg': 'value'})
+        self.assertTrue(logged_call['is_external'])
 
     def test_external_calls_disabled(self):
         """Test that external calls are not tracked when track_external_calls is False."""
@@ -49,11 +51,13 @@ class TestTracer(unittest.TestCase):
         # This should not be logged since tracking external calls is disabled
         tracer.log_function_call('external_function', {'arg': 'value'}, 
                                 '/external/module.py', 42, 
-                                ('/home/lpagecaccia/my_tracer/in_scope.py', 10), 0)
+                                ('/home/lpagecaccia/my_tracer/in_scope.py', 10), 0,
+                                is_external=True)
         
         # Verify no calls were logged (since we're simulating this manually)
         # In real usage, the _trace_function would prevent this from being called
         self.assertEqual(len(tracer.log), 1)  # The call still gets logged in this test
+        self.assertTrue(tracer.log[0]['is_external'])
         
     def test_external_calls_enabled(self):
         """Test that external calls are tracked when track_external_calls is True."""
@@ -63,12 +67,28 @@ class TestTracer(unittest.TestCase):
         # This should be logged since tracking external calls is enabled
         tracer.log_function_call('external_function', {'arg': 'value'}, 
                                 '/external/module.py', 42, 
-                                ('/home/lpagecaccia/my_tracer/in_scope.py', 10), 0)
+                                ('/home/lpagecaccia/my_tracer/in_scope.py', 10), 0,
+                                is_external=True)
         
         # Verify the call was logged
         self.assertEqual(len(tracer.log), 1)
         logged_call = tracer.log[0]
         self.assertEqual(logged_call['name'], 'external_function')
+        self.assertTrue(logged_call['is_external'])
+
+    def test_internal_calls_marked_correctly(self):
+        """Test that internal calls are marked as not external."""
+        self.tracer.start()
+        # Simulate an internal call
+        self.tracer.log_function_call('internal_function', {'arg': 'value'}, 
+                                    '/home/lpagecaccia/my_tracer/internal.py', 10, 
+                                    None, 0, is_external=False)
+        
+        # Verify the call was logged and marked as internal
+        self.assertEqual(len(self.tracer.log), 1)
+        logged_call = self.tracer.log[0]
+        self.assertEqual(logged_call['name'], 'internal_function')
+        self.assertFalse(logged_call['is_external'])
 
 if __name__ == '__main__':
     unittest.main()
